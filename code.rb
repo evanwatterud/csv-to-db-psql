a3b01a359c40dbbd962c8a40722df37115a92636
Evan Watterud <cod.videos@hotmail.com>
2016-03-24 16:02:22 -0400

    functionality complete

--- code.rb
+++ code.rb
@@ -10,3 +10,17 @@ def db_connection
     connection.close
   end
 end
+
+ingredients_csv = CSV.read("ingredients.csv")
+
+db_connection do |conn|
+  ingredients_csv.each do |row|
+    conn.exec("INSERT INTO ingredient_table(ingredient) VALUES ('#{row[1]}')")
+  end
+end
+
+ingredients = db_connection { |conn| conn.exec("SELECT * FROM ingredient_table") }.to_a
+
+ingredients.each do |row|
+  puts "#{row["id"]}. #{row["ingredient"]}"
+end
